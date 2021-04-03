8d892faf5b1c34ddeb0ea7a78932fd62e17408d1
java2124 <rafabf1508@gmail.com>
2021-04-03 20:57:57 -0300

    calculosPreços

--- src/app/booking/booking.page.ts
+++ src/app/booking/booking.page.ts
@@ -1,19 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.page.html',
   styleUrls: ['./booking.page.scss'],
 })
 export class BookingPage implements OnInit {
+
+  public nameHouse = 'Casa Floresta Encantada';
+  public descriptionHouse = 'Casa na floresta em São Roque';
+  public detailsHouse = '6 camas . 3 banheiros';
+  public avaliationHouse = 4.86;
+
   public optionWork = false;
   public editHospd = false;
   public editDate2 = false;
   public finishDate = new Date();
   public initDate = new Date();
   public close = true;
-  public textWork;
+  public textWork2 = false;
+  public savedWork = true;
+  public addCupom = false;
+  public removeButtonCupom = true;
+
+  public priceDay = 70.00;
+  public days = 5;
+  public priceTotalDays = this.priceDay * this.days;
+  public deductionWeek = 20;
+  public deductionWeekTotal = (this.priceTotalDays * (this.deductionWeek * (this.days / 7))) / 100;
+  public cleaningFee = 45.00;
+  public serviceFee = 60.00;
+  public totalCost = (this.priceTotalDays + this.cleaningFee + this.serviceFee) - this.deductionWeekTotal;
+
+  public total1 = this.totalCost;
+  public total2 = this.totalCost / 2;
+  public total3 = this.totalCost / 3;
+  public total4 = this.totalCost / 4;
+  public total5 = this.totalCost / 5;
+
+  public cupom(){
+    this.addCupom = true;
+    this.removeButtonCupom = false;
+  }
+
+  public notCupom(){
+    this.removeButtonCupom = true;
+    this.addCupom = false;
+  }
+
+  public okCupom(){
+    console.log('Cupom adicionado!');
+  }
+
+  public textWork(){
+    this.textWork2 = true;
+    this.savedWork = true;
+  }
+
+  public saveWork(){
+    this.savedWork = false;
+    console.log('Motivo da viagem salvo no banco de dados');
+  }
 
   public editDate(){
     this.editDate2 = true;
@@ -34,6 +83,16 @@ export class BookingPage implements OnInit {
     });
     alert.present();
     console.log('Reserva a ser incluída no banco de dados');
+  }
+
+  public  async moreInfo(){
+    const alert = await this.alertController.create({
+      header: 'Informações de preço',
+      subHeader: 'Por que pago uma Taxa de Serviço?',
+      message: 'Isso nos ajuda a manter nossa plataforma e a oferecer serviços como atendimento 24 horas por dia durante a sua viagem.',
+      buttons: ['Sair']
+    });
+    alert.present();
   }
 
 
